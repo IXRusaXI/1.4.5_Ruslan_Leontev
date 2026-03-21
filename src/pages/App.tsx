d842f2476355346ebc06b5f4ac62c53c0cf4951b
IXRusaXI <123xrusax123@gmail.com>
2026-03-21 22:36:27 +0300

    .

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,9 @@
-import './App.scss';
+import style from './App.module.scss';
 import { TodoList } from './../widgets/TodoList/TodoList.tsx';
 
 const App = () => {
   return (
-    <div className="container">
+    <div className={style.container}>
       <TodoList />
     </div>
   );
